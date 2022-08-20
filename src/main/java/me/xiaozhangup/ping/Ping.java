1a87eddf8d3f71d8a78305e4209701fa5b784b0a
xiaozhangup <xiaozhangshiw@163.com>
2022-08-21 00:09:50 +0800

    容错

--- src/main/java/me/xiaozhangup/ping/Ping.java
+++ src/main/java/me/xiaozhangup/ping/Ping.java
@@ -13,17 +13,21 @@ public class Ping {
         InetAddress address = InetAddress.getByName("tcat.ncgs.top");
         int port = 22139;
         while (true) {
-            ServerPing ping = new ServerPing(new InetSocketAddress(address, port));
-            StatusResponse response = ping.fetchData();
-            double online = response.getPlayers().getOnline();
-            double max = response.getPlayers().getMax();
-            double point = (online / max) * 100;
-            StringBuilder line = new StringBuilder("|");
-            for (int i = 0; i < point; i++) {
-                line.append("|");
+            try {
+                ServerPing ping = new ServerPing(new InetSocketAddress(address, port));
+                StatusResponse response = ping.fetchData();
+                double online = response.getPlayers().getOnline();
+                double max = response.getPlayers().getMax();
+                double point = (online / max) * 100;
+                StringBuilder line = new StringBuilder("|");
+                for (int i = 0; i < point; i++) {
+                    line.append("|");
+                }
+                System.out.println((int) online + "/" + (int) max + " " + line);
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-            System.out.println((int) online + "/" + (int) max + " " + line);
-            Thread.sleep(1000);
         }
     }
 
